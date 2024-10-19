0c62cde6e4a4e5bffe05933ac76241e0a9355dda
Dawid Urbas <durbas92@gmail.com>
2024-10-19 22:44:18 +0200

    Change pdf

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,7 +1,20 @@
+import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 
-export const generatePDFReport = (): void => {
-  const doc = new jsPDF();
-  doc.text("raport", 10, 10);
-  doc.save("raport.pdf");
+const generatePDFReport = async () => {
+  const element = document.getElementById("popup-content");
+  if (!element) return;
+
+  const canvas = await html2canvas(element, { scale: 2 });
+  const imgData = canvas.toDataURL("image/png");
+
+  const pdf = new jsPDF();
+  const imgProps = pdf.getImageProperties(imgData);
+  const pdfWidth = pdf.internal.pageSize.getWidth();
+  const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+
+  pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+  pdf.save("report.pdf");
 };
+
+export { generatePDFReport };
